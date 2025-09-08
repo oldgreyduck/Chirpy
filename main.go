f5a5fc966f775078183040844329767e8eb9b547
Christopher Caquelin <caquelin@hey.com>
2025-09-08 23:12:25 +0000

    CH1 L8 complete chirpy image added

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
