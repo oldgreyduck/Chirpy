710fef0364fc5482938a9c63943101e06111b383
Christopher Caquelin <caquelin@hey.com>
2025-09-10 18:02:52 +0000

    CH1 complete

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	mux.HandleFunc("/healthz", healthz)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 	srv.ListenAndServe()
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
